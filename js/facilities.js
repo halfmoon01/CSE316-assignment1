b4f5995605e246229c99c591bdf065abf218606d
halfmoon01 <onurivit01@gmail.com>
2024-10-10 22:57:06 +0900

    first commit

---
+++ js/facilities.js
@@ -0,0 +1,55 @@
+// <!-- Sanghyun Jun-->
+// <!-- Sanghyun.Jun.1@stonybrook.edu-->
+
+
+const facilities = [
+    {
+        name: "Gym",
+        description: "sports hall",
+        capacity: 4,
+        location: "C1033",
+        availableTo: "Available to all",
+        image: "../AssignImages/gym.jpg"
+    },
+    {
+        name: "Auditorium",
+        description: "The Auditorium Theater",
+        capacity: 30,
+        location: "A234",
+        availableTo: "Available to all",
+        image: "../AssignImages/auditorium.jpg"
+    },
+    {
+        name: "Swimming Pool",
+        description: "aquatic center",
+        capacity: 4,
+        location: "C1033",
+        availableTo: "Available to all",
+        image: "../AssignImages/pool.jpg"
+    },
+    {
+        name: "Seminar Room",
+        description: "lecture hall",
+        capacity: 4,
+        location: "C1033",
+        availableTo: "Available to all",
+        image: "../AssignImages/seminar.jpg"
+    },
+    {
+        name: "Conference Room",
+        description: "meeting space",
+        capacity: 4,
+        location: "C1033",
+        availableTo: "Only for SUNY Korea",
+        image: "../AssignImages/conference.jpg"
+    },
+    {
+        name: "Library",
+        description: "study and read books",
+        capacity: 4,
+        location: "C1033",
+        availableTo: "Only for SUNY Korea",
+        image: "../AssignImages/library.jpg"
+    }
+];
+
